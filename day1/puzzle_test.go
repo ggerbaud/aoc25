995523844d39bee03a0362e8c2fa29588af0ad4c
Guillaume GERBAUD <ggerbaud@mabreizh.fr>
2025-12-02 11:49:35 +0100

    D1

---
+++ day1/puzzle_test.go
@@ -0,0 +1,36 @@
+package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	lines := testData()
+	result := part1(lines)
+	expect := 3
+	if result != expect {
+		t.Fatalf("Part1 returns %d, we expect %d", result, expect)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	lines := testData()
+	result := part2(lines)
+	expect := 6
+	if result != expect {
+		t.Fatalf("Part2 returns %d, we expect %d", result, expect)
+	}
+}
+
+func testData() []string {
+	return []string{
+		"L68",
+		"L30",
+		"R48",
+		"L5",
+		"R60",
+		"L55",
+		"L1",
+		"L99",
+		"R14",
+		"L82",
+	}
+}
